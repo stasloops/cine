da9525b6add35ac25d95597609d7086c0d330188
Stas <sowul789@gmail.com>
2022-04-14 23:42:47 +0300

    update

--- src/store/reducers/Anime/AnimeGet.ts
+++ src/store/reducers/Anime/AnimeGet.ts
@@ -5,7 +5,7 @@ import { fetchAnime, fetchError, fetchLoading, fetchPage } from "./AnimeSlice";
 export const getAnime = (page: any) => {
     return async (dispatch: Dispatch) =>{
         try{
-            const res = await axios.get(page === undefined ? `http://kodikapi.com/list?token=30ef128890b06e03700a3628b91c87c2&with_material_data=true&types=anime-serial&translation_id=609&sort=shikimori_rating&limit=28` : `${page}&with_material_data=true&types=anime-serial&translation_id=609&sort=shikimori_rating&limit=28`)
+            const res = await axios.get(page === undefined ? `https://kodikapi.com/list?token=30ef128890b06e03700a3628b91c87c2&with_material_data=true&types=anime-serial&translation_id=609&sort=shikimori_rating&limit=28` : `${page}&with_material_data=true&types=anime-serial&translation_id=609&sort=shikimori_rating&limit=28`)
             dispatch(fetchAnime(res.data.results))
             dispatch(fetchPage(res.data))
             console.log(res.data);
