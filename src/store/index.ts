375774d8a7ea7692bee9f016f22d1ca56d77c730
Stas <sowul789@gmail.com>
2022-04-26 16:13:49 +0300

    A

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { pokemonApi } from "../services/AnimeService";
 import AnimeSlice from "./reducers/Anime/AnimeSlice";
 import AnimeSearchSlice from "./reducers/AnimeSearch/AnimeSearchSlice";
 
 const rootReducer = combineReducers({
     anime: AnimeSlice,
     animeSearch: AnimeSearchSlice,
-    [pokemonApi.reducerPath]: pokemonApi.reducer
 })
 
 export const setupStore = () => {
     return configureStore({
         reducer: rootReducer,
-        middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware().concat(pokemonApi.middleware),
     })
 }
 
