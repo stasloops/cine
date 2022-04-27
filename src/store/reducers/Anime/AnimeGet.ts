c788d02ca23bd75bb40cab02059cc1bbee5ed8be
Stas <sowul789@gmail.com>
2022-04-27 15:39:08 +0300

    s

--- src/store/reducers/Anime/AnimeGet.ts
+++ src/store/reducers/Anime/AnimeGet.ts
@@ -6,7 +6,7 @@ export const getAnime = (page: undefined, valueSort: string, valueGenres: string
     return async (dispatch: Dispatch) => {
         try {
             dispatch(fetchLoading())
-            const res = await axios.get(page === undefined ? `https://kodikapi.com/list?token=30ef128890b06e03700a3628b91c87c2&with_material_data=true&translation_id=609,739,2068,557,827&limit=35&sort=${valueSort}&anime_genres=${valueGenres}&anime_kind=${valueType}${valueYear.length === 0 ? '' : '&year=' + valueYear}` : `${page}&with_material_data=true&translation_id=609,739,2068,557,827&limit=35&sort=${valueSort}&anime_genres=${valueGenres}&anime_kind=${valueType}${valueYear.length === 0 ? '' : '&year=' + valueYear}`)
+            const res = await axios.get(page === undefined ? `https://kodikapi.com/list?token=30ef128890b06e03700a3628b91c87c2&with_material_data=true&translation_id=609,739,2068,557,827&limit=3&sort=${valueSort}&anime_genres=${valueGenres}&anime_kind=${valueType}${valueYear.length === 0 ? '' : '&year=' + valueYear}` : `${page}&with_material_data=true&translation_id=609,739,2068,557,827&limit=35&sort=${valueSort}&anime_genres=${valueGenres}&anime_kind=${valueType}${valueYear.length === 0 ? '' : '&year=' + valueYear}`)
             dispatch(fetchAnime(res.data.results))
             dispatch(fetchPage(res.data))
             console.log(res.data);
